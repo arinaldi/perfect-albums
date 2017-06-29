7d5d3b5821be97a69eec9bc6714a0d37dbacdd9d
Tony <arinaldi@vt.edu>
2017-06-29 18:42:59 -0500

    Convert <a> to <Link>

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Jumbotron, Grid, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
@@ -12,32 +13,32 @@ function Home() {
         <Grid>
           <Row className="show-grid">
             <Col xs={12} md={3}>
-              <a className="no-color" href="/albums">
+              <Link to="/albums" className="no-color">
                 <i className="fa fa-headphones fa-5x" aria-hidden="true" />
                 <h2>Top Albums</h2>
                 <h4>My favorite albums, by year</h4>
-              </a>
+              </Link>
             </Col>
             <Col xs={12} md={3}>
-              <a className="no-color" href="/songs">
+              <Link to="/songs" className="no-color">
                 <i className="fa fa-music fa-5x" aria-hidden="true" />
                 <h2>Perfect Songs</h2>
                 <h4>Songs that I deem timeless classics</h4>
-              </a>
+              </Link>
             </Col>
             <Col xs={12} md={3}>
-              <a className="no-color" href="/aotd">
+              <Link to="/aotd" className="no-color">
                 <i className="fa fa-instagram fa-5x" aria-hidden="true" />
                 <h2>Album of the Day</h2>
                 <h4>My daily project on Instagram</h4>
-              </a>
+              </Link>
             </Col>
             <Col xs={12} md={3}>
-              <a className="no-color" href="/collection">
+              <Link to="/collection" className="no-color">
                 <i className="fa fa-database fa-5x" aria-hidden="true" />
                 <h2>My Collection</h2>
                 <h4>My searchable collection of albums</h4>
-              </a>
+              </Link>
             </Col>
           </Row>
         </Grid>

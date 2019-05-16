7682851a3d17eb28cfca0dbb48400797f8a23bd0
Tony Rinaldi <tony.rinaldi@silvercar.com>
2019-05-16 15:44:31 -0500

    Add tests for new modal

---
+++ client/src/__tests__/CreateSongModal.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+
+import CreateSongModal from '../components/CreateSongModal';
+import { mockFeaturedSongsData } from '../__mocks__';
+
+const { artist, title, link } = mockFeaturedSongsData[0];
+
+test('CreateSongModal renders with data', () => {
+  const { getByText, getAllByText, getByLabelText } = render(
+    <CreateSongModal
+      isOpen
+      song={mockFeaturedSongsData[0]}
+      isValidated
+      isSaving={false}
+      handleChange={() => {}}
+      handleClose={() => {}}
+      handleSubmit={() => {}}
+      error=''
+    />
+  );
+  const titleHeader = getByText('Create Song');
+  const artistInput = getByLabelText('Artist');
+  const titleInput = getByLabelText('Title');
+  const linkInput = getByLabelText('Link');
+  const closeButtons = getAllByText('Close');
+  const saveButton = getByText('Save');
+
+  expect(titleHeader).toBeInTheDocument();
+  expect(artistInput.value).toBe(artist);
+  expect(titleInput.value).toBe(title);
+  expect(linkInput.value).toBe(link);
+  expect(closeButtons[0]).toBeInTheDocument();
+  expect(closeButtons[1]).toBeInTheDocument();
+  expect(saveButton).toBeInTheDocument();
+});
---
+++ client/src/__tests__/CreateSongModalContainer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+
+import CreateSongModalContainer from '../containers/CreateSongModal';
+import MyProvider from '../containers/MyProvider';
+
+test('CreateSongModalContainer renders', async () => {
+  const { getByText, getByLabelText, getAllByText } = render(
+    <MyProvider>
+      <CreateSongModalContainer
+        isOpen
+        setIsOpen={() => {}}
+        refresh={() => {}}
+      />
+    </MyProvider>
+  );
+  const titleHeader = getByText('Create Song');
+  const artistInput = getByLabelText('Artist');
+  const titleInput = getByLabelText('Title');
+  const linkInput = getByLabelText('Link');
+  const closeButtons = getAllByText('Close');
+  const saveButton = getByText('Save');
+
+  expect(titleHeader).toBeInTheDocument();
+  expect(artistInput.value).toBe('');
+  expect(titleInput.value).toBe('');
+  expect(linkInput.value).toBe('');
+  expect(closeButtons[0]).toBeInTheDocument();
+  expect(closeButtons[1]).toBeInTheDocument();
+  expect(saveButton).toBeInTheDocument();
+});
+
+test('CreateSongModalContainer does not render when closed', async () => {
+  const { queryByText } = render(
+    <MyProvider>
+      <CreateSongModalContainer
+        isOpen={false}
+        setIsOpen={() => {}}
+        refresh={() => {}}
+      />
+    </MyProvider>
+  );
+  const titleHeader = queryByText('Delete Song');
+
+  expect(titleHeader).not.toBeInTheDocument();
+});

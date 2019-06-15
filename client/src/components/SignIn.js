bd334e8a49ffcf8ce8e7713eae0f7327b7962a4b
Tony <arinaldi@vt.edu>
2019-06-14 19:42:14 -0500

    Put SignIn form into grid

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import PropTypes from 'prop-types';
 
@@ -15,42 +16,46 @@ const SignIn = ({
 }) => (
   <Container>
     <h3>Sign In</h3>
-    <Form onSubmit={handleSubmit}>
-      <Form.Row>
-        <Form.Group as={Col} controlId='formUsername'>
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type='text'
-            name='username'
-            autoComplete='username'
-            value={username}
-            onChange={handleChange}
-          />
-        </Form.Group>
-      </Form.Row>
-      <Form.Row>
-        <Form.Group as={Col} controlId='formPassword'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type='password'
-            name='password'
-            autoComplete='current-password'
-            value={password}
-            onChange={handleChange}
-          />
-        </Form.Group>
-      </Form.Row>
-      <Form.Row>
-        <Form.Group as={Col}>
-          <SubmitButton
-            isDisabled={!(username && password) || isSubmitting}
-            isLoading={isSubmitting}
-            text='Submit'
-            loadingText='Submitting...'
-          />
-        </Form.Group>
-      </Form.Row>
-    </Form>
+    <Row>
+      <Col md={{ span: 6, offset: 3 }}>
+        <Form onSubmit={handleSubmit}>
+          <Form.Row>
+            <Form.Group as={Col} controlId='formUsername'>
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type='text'
+                name='username'
+                autoComplete='username'
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} controlId='formPassword'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type='password'
+                name='password'
+                autoComplete='current-password'
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col}>
+              <SubmitButton
+                isDisabled={!(username && password) || isSubmitting}
+                isLoading={isSubmitting}
+                text='Submit'
+                loadingText='Submitting...'
+              />
+            </Form.Group>
+          </Form.Row>
+        </Form>
+      </Col>
+    </Row>
   </Container>
 );
 

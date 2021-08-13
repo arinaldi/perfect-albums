bcf37cf6e20fb79067555e0c99e6c32fb48bf84c
Tony Rinaldi <tony.rinaldi13@gmail.com>
2021-08-13 11:20:34 -0500

    Remove route to fetch album IDs

--- src/controllers/albums/getIds.js
+++
@@ -1,9 +0,0 @@
-const Album = require('../../models/album');
-
-module.exports = () =>
-  new Promise((resolve, reject) => {
-    Album.distinct('_id').exec((err, ids) => {
-      if (err) reject(err);
-      resolve(ids);
-    });
-  });
--- src/routes/public.js
+++ src/routes/public.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 
 const getArtists = require('../controllers/albums/getArtists');
-const getIds = require('../controllers/albums/getIds');
 const { ERRORS } = require('../constants');
 const rateLimiter = require('../middleware/rateLimiter');
 
@@ -13,15 +12,6 @@ router.get('/api/artists', async (_, res) => {
   try {
     const artists = await getArtists();
     res.json(artists.sort());
-  } catch (err) {
-    res.status(500).json({ error: err.message || ERRORS.GENERIC });
-  }
-});
-
-router.get('/api/albums/ids', async (_, res) => {
-  try {
-    const ids = await getIds();
-    res.json(ids);
   } catch (err) {
     res.status(500).json({ error: err.message || ERRORS.GENERIC });
   }
